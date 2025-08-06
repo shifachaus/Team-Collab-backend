98768bd54fbfe5e368c5c41583be33942554c06e
shifachaus <chausshifa804@gmail.com>
2025-08-06 20:34:54 +0530

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import session from "cookie-session";
+import { config } from "./config/app.config";
+
+//App Initialization
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+//Middleware Setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Session Configuration
+app.use(
+  session({
+    name: "session",
+    keys: [config.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000,
+    secure: config.NODE_ENV == "production",
+    httpOnly: true,
+    sameSite: "lax",
+  })
+);
+app.use(
+  cors({
+    origin: config.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    message: "Hello world!",
+  });
+});
+
+app.listen(config.PORT, async () => {
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+});
