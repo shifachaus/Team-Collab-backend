558f972ad64a85228b7db9909d15a0b1b28e6e2a
shifachaus <chausshifa804@gmail.com>
2025-08-11 15:03:23 +0530

    feat: add account model

---
+++ src/models/account.model.ts
@@ -0,0 +1,55 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+import { ProviderEnum, ProviderEnumType } from "../enums/account-provider.enum";
+import { required } from "zod/v4/core/util.cjs";
+
+export interface AccountDocument extends Document {
+  provider: ProviderEnumType;
+  providerId: string; // Store the email, googleId, facebookId as the providerId
+  userId: mongoose.Types.ObjectId;
+  refreshToken: string | null;
+  tokenExpiry: Date | null;
+  createdAt: Date;
+}
+
+const accountSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+
+    provider: {
+      type: String,
+      enum: Object.values(ProviderEnum),
+      required: true,
+    },
+
+    providerId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    refreshToken: {
+      type: String,
+      default: null,
+    },
+    tokenExpiry: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        delete (ret as any).refreshToken;
+      },
+    },
+  }
+);
+
+const AccountModel = mongoose.model<AccountDocument>("Account", accountSchema);
+export default AccountModel;
